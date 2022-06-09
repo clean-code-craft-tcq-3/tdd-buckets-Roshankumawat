299c745ebb35c0ccfec614f5fb3459aa5cfdaf4b
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-06-09 21:51:07 +0530

    Create charging_current_range_test.py

---
+++ charging_current_range_test.py
@@ -0,0 +1,13 @@
+import unittest
+import charging_current_range
+
+
+class RangeBucketTest(unittest.TestCase):
+    def test_range_details(self):
+        self.assertTrue(charging_current_range. checkRange([4, 5]) == 1)
+        self.assertTrue(charging_current_range. checkRange([2, 3, 4,5, 6, 7]) == 6)
+        self.assertTrue(charging_current_range. checkRange([1, 5, 2, 3, 4]) == 5)
+
+    
+if __name__ == '__main__':
+    unittest.main()
