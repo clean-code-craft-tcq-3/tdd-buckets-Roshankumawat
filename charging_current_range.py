8d14ebd8be5480489bd451db05ef08f7a6599dba
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-06-09 21:44:54 +0530

    Update charging_current_range.py

--- charging_current_range.py
+++ charging_current_range.py
@@ -1,1 +1,15 @@
 
+def checkDifference(number1, number2):
+   return number2-number1
+    
+def checkRange(inputArray):
+    reading=1
+    inputArray.sort()
+    first_number1= inputArray[0]
+    for index in range(0, len(inputArray)-1):
+        first_number= inputArray[index]
+        next_number= inputArray[index+1]
+        if (checkDifference(first_number,next_number) ==0 or checkDifference(first_number,next_number)==1):
+            reading=reading+1
+        else:
+            print("Range:",first_number1,"-",first_number, reading )
