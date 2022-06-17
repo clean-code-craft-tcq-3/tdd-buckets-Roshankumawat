2a940efbb9a7c8e41a0ab2d324227b7373ab18f3
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-06-17 20:48:04 +0530

    Update charging_current_range.py

--- charging_current_range.py
+++ charging_current_range.py
@@ -1,31 +1,34 @@
 newArray=[]
-def checkDifference(number1, number2):
-   difference = number2 - number1
-   return (difference == 0 or difference == 1)
-
+def isConsecutive(number1, number2):
+    difference = number2 - number1
+    return (difference == 0 or difference == 1)
 def getRangeDetails(inputArray):
-    itr= 0
     newArray=[]
     inputArray.sort()
     reading=1
     range_first_number=inputArray[0]
     range_end_number= inputArray[0]
-    
 
-    
     for index in range(0, len(inputArray)-1):
-        if(checkDifference(inputArray[index],inputArray[index+1])):#and index<len(inputArray)-1
+       if(isConsecutive(inputArray[index],inputArray[index+1])):
             reading+=1
             range_end_number=inputArray[index+1]
-           
+                    
         else:
-           newArray.append(range_first_number)
-           newArray.append(range_end_number)
-           reading=1
-           range_first_number=inputArray[index+1]
-           range_end_number=inputArray[index+1]
+            newRangeArray=(range_first_number, range_end_number, reading)
+            newArray.append(newRangeArray)
+            reading=1
+            range_first_number=inputArray[index+1]
+            range_end_number=inputArray[index+1]
 
-        newRangeArray=(range_first_number, range_end_number, reading)
-        newArray.append(newRangeArray)
-       
-    return reading 
+    newRangeArray=(range_first_number, range_end_number, reading)
+    newArray.append(newRangeArray)
+    for num in range(0, len(newArray)-1):
+        formateRange(newArray[num])
+        
+def formateRange(num):
+    # print("in Formate function")
+    num= print(num[0],"-", num[1],",", num[2])
+    return num
+
+
