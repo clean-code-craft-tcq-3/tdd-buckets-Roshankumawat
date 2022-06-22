b836a4be6b5553ce3b33b42e9a5524f07d1c85e5
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-06-22 15:04:09 +0530

    Update charging_current_range_test.py

--- charging_current_range_test.py
+++ charging_current_range_test.py
@@ -2,7 +2,7 @@ import unittest
 import charging_current_range
 class RangeBucketTest(unittest.TestCase):
     def test_range_details(self):
-        self.assertTrue(getRangeDetails([5, 5,6,7, 8, 1, 2, 2, 1, 1,2])==[(1 , 2 , 6), (5 , 8, 5)])
+        self.assertTrue(charging_current_range.getRangeDetails([5, 5,6,7, 8, 1, 2, 2, 1, 1,2])==[(1 , 2 , 6), (5 , 8, 5)])
         self.assertTrue(charging_current_range.getRangeDetails([4, 5]) ==[(4 , 5 , 2)])
         self.assertTrue(charging_current_range.getRangeDetails([2, 3, 4, 6, 7]) == [(2 , 4 , 3), (6 , 7 , 2)])
    
