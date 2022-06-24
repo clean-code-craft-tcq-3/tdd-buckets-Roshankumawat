7ed8bdc4f44bc25d609198aa656b35f9a6da7e7e
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-06-24 10:47:06 +0530

    Update charging_current_range_test.py

--- charging_current_range_test.py
+++ charging_current_range_test.py
@@ -2,7 +2,7 @@ import unittest
 import charging_current_range
 class RangeBucketTest(unittest.TestCase):
     def test_binary_to_decimal(self):
-        self.assertTrue(Charging_current_range.binary_to_decimal("1111111111"==1023))
+        self.assertTrue(charging_current_range.binary_to_decimal("1111111111"==1023))
                         
     def test_range_details(self):
         self.assertTrue(charging_current_range.getRangeDetails([5, 5,6,7, 8, 1, 2, 2, 1, 1,2])==[(1 , 2 , 6), (5 , 8, 5)])
