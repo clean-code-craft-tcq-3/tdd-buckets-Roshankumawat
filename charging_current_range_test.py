c7d3d6fe404256bc42b19aaf8bc629fac8334cdf
Roshankumawat <34167471+Roshankumawat@users.noreply.github.com>
2022-07-03 12:48:21 +0530

    Update charging_current_range_test.py

--- charging_current_range_test.py
+++ charging_current_range_test.py
@@ -1,5 +1,7 @@
 import unittest
 import charging_current_range
+import Current_sensing_at_high_fidility
+
 class RangeBucketTest(unittest.TestCase):
     def test_binary_to_decimal(self):
         self.assertTrue(charging_current_range.binary_to_decimal("1111111111")==1023)
@@ -8,6 +10,8 @@ class RangeBucketTest(unittest.TestCase):
         self.assertTrue(charging_current_range.getRangeDetails([5, 5,6,7, 8, 1, 2, 2, 1, 1,2])==[(1 , 2 , 6), (5 , 8, 5)])
         self.assertTrue(charging_current_range.getRangeDetails([4, 5]) ==[(4 , 5 , 2)])
         self.assertTrue(charging_current_range.getRangeDetails([2, 3, 4, 6, 7]) == [(2 , 4 , 3), (6 , 7 , 2)])
-   
+        
+   def test_sensor_output(self):
+        self.assertTrue(Current_sensing_at_high_fidility.decimal_to_binary(123)==(['0','0','0', '0', '0', '1', '1', '1', '1', '0', '1', '1'])
 if __name__ == '__main__':
     unittest.main()
